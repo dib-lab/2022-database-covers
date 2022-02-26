b22ea3cf7701b75f196c09a14abc4f32b2b768a8
C. Titus Brown <titus@idyll.org>
2022-02-26 05:06:47 -0800

    upd

--- make-db-cover.py
+++ make-db-cover.py
@@ -3,6 +3,7 @@ import sys
 import sourmash
 from sourmash import sourmash_args
 import argparse
+import os.path
 
 
 from sourmash.search import JaccardSearch, SearchType
@@ -20,28 +21,38 @@ class MyJaccardSearch(JaccardSearch):
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('database')
+    p.add_argument('databases', nargs='+')
     p.add_argument('--scaled', default=10000, type=int)
     p.add_argument('-o', '--output', required=True)
     args = p.parse_args()
 
     seen_hashes = set()
 
-    idx = sourmash.load_file_as_index(args.database)
-
     with sourmash_args.SaveSignaturesToLocation(args.output) as save_sigs:
-        for n, ss in enumerate(idx.signatures()):
-            print(n, len(seen_hashes))
-            mh = ss.minhash.downsample(scaled=args.scaled)
-            new_mh = mh.copy_and_clear()
+        for filename in args.databases:
+            idx = sourmash.load_file_as_index(filename)
 
-            hashes = set(mh.hashes)
-            remaining = hashes - seen_hashes
-            new_mh.add_many(remaining)
-            seen_hashes.update(remaining)
+            for n, ss in enumerate(idx.signatures()):
+                print(f"examining {ss.name[:30]}... from {os.path.basename(filename)} - #{n}, {len(seen_hashes)} total hashes.")
+                mh = ss.minhash.downsample(scaled=args.scaled)
 
-            ss.minhash = new_mh
-            save_sigs.add(ss)
+                hashes = set(mh.hashes)
+                remaining = hashes - seen_hashes
+                seen_hashes.update(remaining)
+
+                if len(hashes) == len(remaining):
+                    print("saving unmodified signature.")
+                    save_sigs.add(ss)
+                elif len(remaining) == 0:
+                    print("DISCARDING signature.")
+                else:
+                    new_mh = mh.copy_and_clear()
+
+                    new_mh.add_many(remaining)
+                    ss.minhash = new_mh
+                    save_sigs.add(ss)
+                    print(f'saving updated minhash ({len(hashes)} to {len(remaining)})')
+                print('---')
 
 
 if __name__ == '__main__':
