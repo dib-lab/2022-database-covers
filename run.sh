e98ef895f930d6e36c7ec1b03516bfa573ae7bb9
C. Titus Brown <titus@idyll.org>
2023-09-06 14:28:17 -0700

    work around sourmash#2751

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 
 /usr/bin/time -v ./make-db-cover.py /group/ctbrowngrp/sourmash-db/gtdb-rs214/gtdb-rs214-reps.k21.zip \
     /group/ctbrowngrp/sourmash-db/gtdb-rs214/gtdb-rs214-k21.zip \
-    -o gtdb-rs214.k21.cover.d/ --scaled=10000 -k 21
+    -o gtdb-rs214.k21.cover.zip --scaled=10000 -k 21
+
+mkdir -p gtdb-rs214.k21.cover.d
+cd gtdb-rs214.k21.cover.d && sourmash sig split ../gtdb-rs214.k21.cover.zip -E .sig.gz && cd ..
+
+find $(pwd)/gtdb-rs214.k21.cover.d -type f > list.gtdb-rs214.k21.cover.txt
