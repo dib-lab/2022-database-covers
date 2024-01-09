b9620db43cdedb00afde75eb44698b0db04128f0
C. Titus Brown <titus@idyll.org>
2024-01-09 06:12:57 -0800

    adjust to scaled=1000; support --preload

--- make-db-cover.py
+++ make-db-cover.py
@@ -12,14 +12,29 @@ def main():
     p.add_argument('--scaled', default=10000, type=int)
     p.add_argument('-k', '--ksize', default=31, type=int)
     p.add_argument('-o', '--output', required=True)
+    p.add_argument('--preload', help="load this cover first")
     args = p.parse_args()
 
     seen_hashes = set()
 
+    if args.preload:
+        print(f"preloading '{args.preload}'")
+        pre = sourmash.load_file_as_index(args.preload)
+        pre = pre.select(ksize=args.ksize)
+
+        for n, ss in enumerate(pre.signatures()):
+            if n % 10000 == 0:
+                print(f'preload at {n}')
+            mh = ss.minhash.downsample(scaled=args.scaled)
+            hashes = set(mh.hashes)
+            seen_hashes.update(hashes)
+
+        print(f"preloaded {len(seen_hashes)} hashes.")
+
     with sourmash_args.SaveSignaturesToLocation(args.output) as save_sigs:
         print(f"opening '{args.output} to save.")
         for filename in args.databases:
-            print(F"opening '{filename} for reading...")
+            print(f"opening '{filename} for reading...")
             idx = sourmash.load_file_as_index(filename)
             idx = idx.select(ksize=args.ksize)
 
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
+set -e
+set -x
 
 /usr/bin/time -v ./make-db-cover.py /group/ctbrowngrp/sourmash-db/gtdb-rs214/gtdb-rs214-reps.k21.zip \
     /group/ctbrowngrp/sourmash-db/gtdb-rs214/gtdb-rs214-k21.zip \
-    -o gtdb-rs214.k21.cover.zip --scaled=10000 -k 21
+    -o gtdb-rs214.k21.cover.zip --scaled=1000 -k 21
 
-mkdir -p gtdb-rs214.k21.cover.d
-cd gtdb-rs214.k21.cover.d && sourmash sig split ../gtdb-rs214.k21.cover.zip -E .sig.gz && cd ..
+mkdir -p gtdb-rs214.k21.scaled=1000.cover.d
+cd gtdb-rs214.k21.scaled=1000.cover.d && sourmash sig split ../gtdb-rs214.k21.cover.zip -E .sig.gz && cd ..
 
-find $(pwd)/gtdb-rs214.k21.cover.d -type f > list.gtdb-rs214.k21.cover.txt
+find $(pwd)/gtdb-rs214.k21.scaled=1000.cover.d -type f > list.gtdb-rs214.k21.cover.txt
