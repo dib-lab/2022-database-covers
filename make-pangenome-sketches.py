abd604ef59aa22e44a201948e2cea75896bdc743
C. Titus Brown <titus@idyll.org>
2024-01-09 07:26:24 -0800

    rough draft? ;)

--- make-pangenome-sketches.py
+++ make-pangenome-sketches.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import argparse
 import sys
+
 from collections import defaultdict
 import sourmash
 from sourmash import sourmash_args
@@ -27,7 +28,7 @@ def main():
     print(f"loading taxonomies from {args.taxonomy_file}")
     taxdb = sourmash.tax.tax_utils.MultiLineageDB.load(args.taxonomy_file)
 
-    revtax_d = defaultdict(set)
+    revtax_d = {}
     ident_d = {}
 
     for filename in args.sketches:
@@ -36,17 +37,41 @@ def main():
         db = db.select(ksize=args.ksize)
         mf = db.manifest
         assert mf, "no matching sketches for given ksize!?"
-        for row in mf.rows:
-            name = row['name']
-            md5sum = row['md5']
+
+        for ss in db.signatures():
+            name = ss.name
+            md5sum = ss.md5sum()
+
             ident = tax_utils.get_ident(name)
+
+            # grab relevant lineage name
             lineage_tup = taxdb[ident]
             lineage_tup = tax_utils.RankLineageInfo(lineage=lineage_tup)
             lineage_pair = lineage_tup.lineage_at_rank(args.rank)
             lineage_name = lineage_pair[-1].name
-            ident_d[ident] = lineage_name # ok if overwrite...
-            revtax_d[lineage_name].add(row['md5'])
-    
+
+            # pick an ident to represent this set of pangenome sketches
+            ident_d[lineage_name] = ident # ok if overwrite...
+
+            # track
+            mh = revtax_d.get(lineage_name)
+            if mh is None:
+                mh = ss.minhash.to_mutable()
+                revtax_d[lineage_name] = mh
+            else:
+                mh += ss.minhash
+
+            print('...', name)
+
+    with sourmash_args.SaveSignaturesToLocation(args.output) as save_sigs:
+        for lineage_name, ident in ident_d.items():
+            sig_name = f'{ident} {lineage_name}'
+            mh = revtax_d[lineage_name]
+            ss = sourmash.SourmashSignature(mh, name=sig_name)
+            save_sigs.add(ss)
+
+            print(f'saved: {sig_name}')
+
 
 if __name__ == '__main__':
     sys.exit(main())
